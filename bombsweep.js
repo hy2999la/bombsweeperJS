0a97e8065b647669f6564d1b15c6715aa69c1c46
Anson Lam <ansonlam82@gmail.com>
2018-12-22 19:34:52 -0500

    Finished gui and left click events for bomb and numcells

--- bombsweep.js
+++ bombsweep.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("canvas"),
-    contxt = canvas.getContext("2d"),
+    //contxt = canvas.getContext("2d"),
     //Size of canvas
     width = 500,
     height = 500,
@@ -12,51 +12,67 @@ var canvas = document.getElementById("canvas"),
 //The game board which displays the number in each cell from 0 ~ 8 and -1 denoting a bomb
 var board = new Array(columns).fill().map(() => new Array(rows).fill(0));
 
-canvas.width = width;
-canvas.height = height;
-
 var counter = 0;
 var bombNum = 0;
 
 genBombs();
+drawBoard();
 
 function genBombs() {
     for (var i = 0; i < maxBombs; i++) {
-        var x = Math.floor(Math.random() * Math.floor(columns));
-        var y = Math.floor(Math.random() * Math.floor(rows));
-        console.log(x + " " + y);
+        var row = Math.floor(Math.random() * Math.floor(rows));
+        var col = Math.floor(Math.random() * Math.floor(columns));
+        console.log(col + " " + row);
 
         //If chosen tile is not a bomb then we set it to be a bomb and we increment the surrounding tiles' number
-        if (board[x][y] != -1) {
-            board[x][y] = -1;
+        if (board[col][row] != -1) {
+            board[col][row] = -1;
 
-            //Checking for boundary issues and if adjacent tile is a bomb or not
-            if (x > 0) {
-                if (board[x-1][y] != -1) board[x-1][y]++;
-                if (y > 0) {
-                    if (board[x-1][y-1] != -1) board[x-1][y-1]++;
-                    if (board[x][y-1] != -1) board[x][y-1]++;
-                }
-                if (y < rows - 1) {
-                    if (board[x-1][y+1] != -1) board[x-1][y+1]++;
-                    if (board[x][y+1] != -1) board[x][y+1]++;
-                }
-            }
-            if (x < columns - 1) {
-                if (board[x+1][y] != -1) board[x-1][y]++;
-                if (y > 0) {
-                    if (board[x+1][y-1] != -1) board[x+1][y-1]++;
-                }
-                if (y < rows - 1) {
-                    if (board[x+1][y+1] != -1) board[x+1][y+1]++;
-                }
-            }
+            //Increment the surrounding tiles by 1
+                if (col-1>=0 && row-1>=0) { if (board[col-1][row-1] != -1) board[col-1][row-1]++; }
+                if (col-1>=0 && row>=0) { if (board[col-1][row] != -1) board[col-1][row]++; }
+                if (col-1>=0 && row+1<rows) { if (board[col-1][row+1] != -1) board[col-1][row+1]++; }
+                if (col>=0 && row-1>0) { if (board[col][row-1] != -1) board[col][row-1]++; }
+                if (col>=0 && row+1<rows) { if (board[col][row+1] != -1) board[col][row+1]++; }
+                if (col+1<columns && row-1>=0) { if (board[col+1][row-1] != -1) board[col+1][row-1]++; }
+                if (col+1<columns && row>=0) { if (board[col+1][row] != -1) board[col+1][row]++; }
+                if (col+1<columns && row+1<rows) { if (board[col+1][row+1] != -1) board[col+1][row+1]++; }
+
+        } else {
+            i--;
         }
     }
 
     console.table(board);
 }
 
-function drawGrid() {
+function drawBoard() {
+    var grid = clickGrid(function(xell, row, col) {
+        xell.innerHTML = board[row][col];
+        if (board[row][col] == -1) {
+            xell.className = "bomb";
+        } else {
+            xell.className = "numCell";
+        }
+    })
 
+    document.body.appendChild(grid);
+}
+
+function clickGrid(callback) {
+    var grid = document.createElement("table");
+    grid.className = 'board';
+    for (var i = 0; i < columns; i++) {
+        var col = grid.appendChild(document.createElement("tr"));
+        for (var j = 0; j < rows; j++) {
+            var cell = col.appendChild(document.createElement('td'));
+            cell.addEventListener("click", (function(xell, j, i) {
+                return function(){
+                    callback(xell,j,i);
+                }
+            })(cell,j,i),false);
+        }
+    }
+
+    return grid;
 }
