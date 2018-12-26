09d5744f14517c9f42f471fe376624a4f2ee5fb3
ansonidea <anson@ideanotion.net>
2018-12-26 16:49:30 -0500

    click empty reveals neighb empty, reveals all bombs after loss

--- bombsweep.js
+++ bombsweep.js
@@ -2,12 +2,13 @@
 var columns = 15;
 var rows = 15;
 
-var maxBombs = 10;
+var maxBombs = 5;
+const MAX_ALLOW_BOMBS = 25;
 var numFlags = 0;
 
 //The game board which displays the number in each cell from 0 ~ 8 and -1 denoting a bomb
 var board = Array(columns).fill().map(() => Array(rows).fill(0));
-
+var zeroBoard = Array(columns).fill().map(() => Array(rows));
 var counter = 0;
 var bombNum = 0;
 
@@ -16,7 +17,31 @@ drawBoard();
 
 function updateInput(val, id) {
     document.getElementById(id).value=val;
+    setMaxBombIn();
 }
+
+function setMaxBombIn() {
+    var x = +document.getElementById("xSize").value;
+    var y = +document.getElementById("ySize").value;
+    var bombs = document.getElementById("bombNum");
+    var bombsT = document.getElementById("bIn");
+    var bombLabel = document.getElementById("maxBombs");
+
+    if ((x * y) >= MAX_ALLOW_BOMBS) {
+        bombs.max = MAX_ALLOW_BOMBS;
+        bombLabel.innerHTML = MAX_ALLOW_BOMBS;
+    } else {
+        bombs.max = (x*y) - 1;
+        bombLabel.innerHTML = (x*y)-1;
+    }
+    var val = +bombs.value;
+
+    if (val >= bombs.max) {
+        bombs.value = bombs.max;
+        bombsT.value = bombs.max;
+    }
+}
+
 function generateGame() {
     var game = document.getElementById("board");
     if (game) game.parentElement.removeChild(game);
@@ -33,9 +58,9 @@ function generateGame() {
 }
 
 function genBombs() {
-    console.log("columns: " + columns + " rows: " + rows);
     board = Array(columns).fill().map(() => Array(rows).fill(0));
-    console.table(board);
+    zeroBoard = Array(columns).fill().map(() => Array(rows));
+
     for (var i = 0; i < maxBombs; i++) {
         var row = Math.floor(Math.random() * Math.floor(rows));
         var col = Math.floor(Math.random() * Math.floor(columns));
@@ -59,7 +84,6 @@ function genBombs() {
         }
     }
     counter = maxBombs;
-    console.table(board);
 }
 
 function drawBoard() {
@@ -70,6 +94,17 @@ function drawBoard() {
                     el.className = "bomb";
                 } else {
                     el.className = "numCell";
+                    switch (el.innerHTML) {
+                        case '0': openZero(el); break;
+                        case '1': el.classList.add("one"); break;
+                        case '2': el.classList.add("two"); break;
+                        case '3': el.classList.add("three"); break;
+                        case '4': el.classList.add("four"); break;
+                        case '5': el.classList.add("five"); break;
+                        case '6': el.classList.add("six"); break;
+                        case '7': el.classList.add("seven"); break;
+                        case '8': el.classList.add("eight"); break;
+                    }
                 }
             }
             if (el.innerHTML == -1) {
@@ -114,6 +149,7 @@ function createGrid(callback) {
             var cell = col.appendChild(document.createElement('td'));
             cell.innerHTML = board[j][i];
             cell.className = "hidden";
+            zeroBoard[j][i] = cell;
             cell.onclick = function(ev) {
                 return callback(this, ev);
             }
@@ -122,26 +158,84 @@ function createGrid(callback) {
 
     return grid;
 }
+function openZero(el) {
+    var x,y;
+    var search = -1;
+    for (var i = 0; i < zeroBoard.length; i++) {
+        search = zeroBoard[i].indexOf(el);
+        if (search != -1) {
+            x = i;
+            y = search;
+            break;
+        }
+    }
+    if (search == -1) return;
+    openSurZero(x, y);
+}
+
+function openSurZero(x, y) {
+    if (x < 0 || x >= columns || y < 0 || y >= rows) return;
+    var el = zeroBoard[x][y];
+    if (el.className == "numCell zero") return;
+    if (el.innerHTML != 0) {
+        if (el.className != "flagged") {
+            el.className = "numCell";
+            switch (el.innerHTML) {
+                case '1': el.classList.add("one"); break;
+                case '2': el.classList.add("two"); break;
+                case '3': el.classList.add("three"); break;
+                case '4': el.classList.add("four"); break;
+                case '5': el.classList.add("five"); break;
+                case '6': el.classList.add("six"); break;
+                case '7': el.classList.add("seven"); break;
+                case '8': el.classList.add("eight"); break;
+            }
+        }
+    }
+    else {
+        el.className = "numCell zero";
+        //sides
+        openSurZero(x-1, y);
+        openSurZero(x, y-1);
+        openSurZero(x, y+1);
+        openSurZero(x+1, y);
+
+        //corners
+        openSurZero(x-1, y-1);
+        openSurZero(x-1, y+1);
+        openSurZero(x+1, y-1);
+        openSurZero(x+1, y+1);
+    }
+}
 
 function loseGame() {
+    for (var i = 0; i < zeroBoard.length; i++) {
+        for (var j = 0; j < zeroBoard[i].length; j++) {
+            if (zeroBoard[i][j].innerHTML == -1) {
+                zeroBoard[i][j].className = "bomb";
+            }
+        }
+    }
     var endGameMsg = document.createElement("div");
     endGameMsg.appendChild(document.createElement("H2").appendChild(document.createTextNode("You Lost")));
     endGameMsg.appendChild(document.createElement("br"));
     endGameMsg.appendChild(document.createElement("H3").appendChild(document.createTextNode("Click Generate to start a new game")));
     endGameMsg.id = "endgamemsg";
-    document.getElementById("game").appendChild(endGameMsg);
+    endGameMsg.className = "game";
+    document.body.appendChild(endGameMsg);
 
     var gameBoard = document.getElementById("board");
     gameBoard.classList.add("blockClicks");
 }
 
-function winGame() {
+function winGame() {    
     var endGameMsg = document.createElement("div");
     endGameMsg.appendChild(document.createElement("H2").appendChild(document.createTextNode("Congratulations You Won!")));
     endGameMsg.appendChild(document.createElement("br"));
     endGameMsg.appendChild(document.createElement("H3").appendChild(document.createTextNode("Click Generate to start a new game")));
     endGameMsg.id = "endgamemsg";
-    document.getElementById("game").appendChild(endGameMsg);
+    endGameMsg.className = "game";
+    document.body.appendChild(endGameMsg);
 
     var gameBoard = document.getElementById("board");
     gameBoard.classList.add("blockClicks");
